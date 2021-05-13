fc2705b1e070662ba862214a0b34144cb257a502
BeefaloKing <9630438+BeefaloKing@users.noreply.github.com>
2021-05-12 19:26:49 -0600

    First graphics window
    Simply creates a single window then closes after 2 seconds. Any errors
    are printed to stdout before exiting.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,33 @@
 #include <SDL2/SDL.h>
-#include <iostream>
+#include <cstdio>
+
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello, world!\n";
+	SDL_Window* window = nullptr;
+	SDL_Surface* screen = nullptr;
+
+	if (SDL_Init(SDL_INIT_VIDEO))
+	{
+		printf("Unable to initialize SDL: %s\n", SDL_GetError());
+		return 1;
+	}
+
+	window = SDL_CreateWindow("Rebirth", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == nullptr)
+	{
+		printf("Unable to create window: %s\n", SDL_GetError());
+		SDL_Quit();
+		return 1;
+	}
+
+	screen = SDL_GetWindowSurface(window);
+	SDL_FillRect(screen, nullptr, SDL_MapRGB(screen->format, 0xC0, 0xFF, 0xEE));
+	SDL_UpdateWindowSurface(window);
+	SDL_Delay(2000);
+
 	return 0;
 }
